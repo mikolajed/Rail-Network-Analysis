701b3c8a99529c52586d2abf7f9eab3df2d829f8
mjedrze5 <01168836@pw.edu.pl>
2022-04-19 12:41:24 +0200

    graph should be included as a libarary not hpp

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include"graph.hpp"
+
+using namespace std;
+
+int main () {
+	Graph G;
+	G.add_node("a", "a");
+	G.add_node("b", "b");
+	G.add_edge("a", "b", 1);
+	G.print();
+	return 0;
+}
