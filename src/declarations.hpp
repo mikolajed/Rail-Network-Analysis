b4e565811e13e5188ec2b6a06bec746a5609f6bf
mjedrze5 <01168836@pw.edu.pl>
2022-04-29 18:15:06 +0200

    Fix major bug with pointer
    Pointer nor iterators should not be used, since the vector might reallocate memory

--- src/declarations.hpp
+++ src/declarations.hpp
@@ -9,35 +9,43 @@ class Railway {
 	using namespace std;
 	typedef time_t Time;
 	
-	class Ticket {
-	public:
+	struct Ticket {
 		int id;
 		double price;
 		int seat_number;
-		Time purchase_time;
+		Time purchase_date;
 	};
 	vector<Ticket> ticket_log;
 	
-	class Station {
-	public:
+	struct Station {
 		string code;	// aberviation of station name used by railway operator
 		string name;	// full name of the station
+		// other data could be added
 	};
 	vector<Station> stations;
 
 	class Connection {
-	public:
-		Station* origin;
-		Station* dest; 	
+		int origin;	// index of origin station in stations vector
+		int dest; 	// index of destination station in stations vector	
 		Time arrvial_time;
+		friend class Railway;
+	public:
+		Station& get_origin_station();
+		Station& get_dest_station();
+		string get_time_date();		// returns only time of arrival in format dd:mm:ss
+		string get_day_date();		// returns day of arrivial time, e.g. "Monday"
 	};
 	vector<Connection> connections;
 	
 	class Route {
-	public:
-		int train_no; 					// an identifier of a route
-		vector<Connection*> parts;
+		int origin;	// index of origin station in stations vector
+		int dest; 	// index of destination station in stations vector	
+		friend class Railway;	
+	public:	
+		int train_no; 	// an identifier of a route
 		string days;
+		Station& get_origin_station();
+		Station& get_dest_station();
 	};
 	vector<Route> routes;
 	
@@ -46,6 +54,7 @@ class Railway {
 	// nodes are represented as stations and edges as connections 
 	// there is a connection between two nodes if there exist at least one route between them
 	class TrainNetwork {
+		typedef t_time Time;
 		class Node {
 			set<int> adj_in;	// stores list of incoming connections 
 			set<int> adj_out;	// stores list of outgoing connections
@@ -53,19 +62,16 @@ class Railway {
 		};
 		class Edge {
 			int route_id;		// there might be many connections between two stations
-			int v_idx, u_idx; 	// origin and destination station
+			int v_idx, u_idx; 	// indexes of nodes in nodes vector
 			friend class TrainNetwork;
 			Time arrvial_time;
 		};
-
-		vector<Node> V;
-		vector<Edge> E;
+		vector<Node> nodes;
+		vector<Edge> edges;
 		unordered_map<string, int> node_IDs;	// node label is mapped to a number - the index in the vector of nodes
-		
 	public:	
-		// a station has to be added prior to adding a connection between that station and another
-		void add_node(string code);
-		// a connection can be added only between two added stations
+		void add_node(string label);
+		// adding an edge between nodes can be done ONLY IF both nodes exist in the graph
 		void add_edge(int route_id, string origin_code, string dest_code, Time arrival_time);
 		vector<string> shortest_path(string origin_code, string dest_code);
 	};
